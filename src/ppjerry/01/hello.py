d977e74ac2ad12373ab431bddc0d175f0eb03880
ppjerry <ppjerry@163.com>
2016-08-03 20:08:55 +0800

    this is to test the github add function

---
+++ src/ppjerry/01/hello.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"this script is just to test the github add function"
+
+print("Hello, world!")
+
+"""
+import sys
+
+class Hello:
+ Demonstration for quick start
+  without parameter, will print usage
+  ./hello
+  > Usage:
+    ./hello [prefix] UserName
+  with only one parameter, will use default prefix: Hello
+  ./hello Joe
+  > Hello Joe
+  two parameter
+  ./hello Hallo Joe
+  > Hallo Joe
+  
+  def __init__(self, prefix="Hello"):
+    self.prefix = prefix
+
+  def sayHello(self, name):
+    print("%s %s" % (self.prefix, name))
+
+if __name__ == "__main__":
+  argv = sys.argv
+  args = len(argv)
+
+  if args == 1:
+    print("Usage: \n\t./hello [prefix] UserName")
+  elif args == 2:
+    Hello().sayHello(argv[1])
+  else:
+    Hello(argv[1]).sayHello(argv[2])
+"""
